5859ab517adb2cda986cd8fdfd69100a30bcfa73
PablodlFR <alu0101336152@ull.edu.es>
2023-03-16 17:38:47 +0000

    first commit

---
+++ tests/ej-clase.spec.ts
@@ -0,0 +1,8 @@
+import "mocha";
+import { expect } from "chai";
+
+// describe("Singleton test", () => {
+//   it("equal instance returns true", () => {
+//     expect(prueba === prueba2).to.be.eql(true);
+//   });  
+// });
